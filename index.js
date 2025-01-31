f84e05125179b18d702b8fb64ffe143d809b0b1f
MaybeSomeone-arc18 <maybesomeone7483@gmail.com>
2025-01-31 11:12:04 +0530

    assignment

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
+require('dotenv').config();
 const express = require('express');
 const { resolve } = require('path');
+const mongoose = require('mongoose');
 
 const app = express();
-const port = 3010;
+const port = process.env.PORT || 3010;
+const MONGO_URI = process.env.MONGO_URI;
 
 app.use(express.static('static'));
 
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
 });
+
+// Connect to MongoDB
+mongoose.connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log('Connected to database'))
+.catch(err => console.error(`Error connecting to database: ${err.message}`));
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
